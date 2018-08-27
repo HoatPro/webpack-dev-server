76cb903a8b76ff8cc057b2ba5f3760171fd36c15
HoatPro <nguyenvanhoat.ict@gmail.com>
2018-08-27 15:33:43 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path=require('path');
+const webpack=require('webpack');
+const VENDOR_LIBS =[
+    "axios",
+    "bootstrap",
+    "jquery",
+    "popper.js",
+    "react",
+    "react-dom",
+    "react-popper",
+    "react-redux",
+    "react-router-dom",
+    "redux",
+]
+module.exports={
+     entry :{
+         bundle:'./src/index.js',
+         vendor :VENDOR_LIBS
+     },
+     output:{
+         path :path.join(__dirname,'dist'),
+         filename:'[name].js'
+     },
+     module:{
+         rules:[
+             {
+                 use:'babel-loader',
+                 test:/\.js$/,
+                 exclude:'/node_modules/'
+             },
+             {
+                 use :[
+                     'style-loader',
+                     'css-loader'
+                 ],
+                 test:/\.css$/
+             },
+             {
+                 loader:'file-loader',
+                 test:/\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.woff2$|\.eot$|\.ttf$|\.wav$|\.mp3$|\.ico$/
+             }
+         ]
+     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$':'jquery',
+            'jQuery':'jquery',
+            'windown.$':'iquery',
+            'windown.jQuery':'jquery'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'vendor'
+        })
+    ]
+}
